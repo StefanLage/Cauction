90bba3258d23ed4cb4c82918fc32528e5589295d
Stefan Lage <lagestfan@gmail.com>
2017-11-10 19:59:31 +0000

    Init clean project

---
+++ Cauction/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  Cauction
+//
+//  Created by Stefan Lage on 08/11/2017.
+//  Copyright © 2017 Stefan Lage. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:[UIViewController new]];
+    self.window.rootViewController = navigationController;
+    
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
---
+++ Cauction/Supporting Files/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Cauction
+//
+//  Created by Stefan Lage on 08/11/2017.
+//  Copyright © 2017 Stefan Lage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
