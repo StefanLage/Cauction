47179bdac091a916cd932943c9c7c5494b0936c6
Stefan Lage <lagestfan@gmail.com>
2017-11-12 16:13:05 +0000

    Refactoring tests

--- CauctionTests/CAuctionsViewModelTests.m
+++ CauctionTests/CAuctionsViewModelTests.m
@@ -48,20 +48,20 @@
     [super tearDown];
 }
 
-- (void)testCauctionsViewModelReturnsAuctions {
+- (void)testWithExpectationCount:(NSUInteger)expCount initHandler:(nonnull void (^)(CAuctionsViewModel * viewModel))initHandler completion:(nonnull void (^)(CAuctionsViewModel * viewModel))completionHandler{
     XCTestExpectation *exp = [self expectationWithDescription:@"testCauctionViewModelReturnsAuctions"];
-    [exp setExpectedFulfillmentCount:3];
+    [exp setExpectedFulfillmentCount:expCount];
     __block NSUInteger initAuctionsCount = 0;
+    NSUInteger initRecurrentValue = expCount - 1;
     CAuctionsViewModel *viewModel = [[CAuctionsViewModel alloc] initWithApiClient:self.mockApiClient];
     [[viewModel.auctionsObserver deliverOnMainThread]
      subscribeNext:^(id _) {
-         if (initAuctionsCount < 2){
-             // First two calls are just initialising the array
+         if (initAuctionsCount < initRecurrentValue){
              initAuctionsCount++;
-             XCTAssertEqual([viewModel auctionsInSection:0], 0);
+             initHandler(viewModel);
          }
          else{
-             XCTAssertEqual([viewModel auctionsInSection:0], 2);
+             completionHandler(viewModel);
          }
          [exp fulfill];
      }];
@@ -70,60 +70,35 @@
                                  handler:^(NSError * _Nullable error) {
                                      XCTAssertNil(error);
                                  }];
+}
+
+- (void)testCauctionsViewModelReturnsAuctions {
+    [self testWithExpectationCount:3 initHandler:^(CAuctionsViewModel * viewModel){
+        XCTAssertEqual([viewModel auctionsInSection:0], 0);
+    } completion:^(CAuctionsViewModel * viewModel){
+        XCTAssertEqual([viewModel auctionsInSection:0], 2);
+    }];
 }
 
 - (void)testCauctionsViewModelAuctionName {
-    XCTestExpectation *exp = [self expectationWithDescription:@"testCauctionViewModelAuctionName"];
-    [exp setExpectedFulfillmentCount:3];
-    __block NSUInteger initAuctionsCount = 0;
-    CAuctionsViewModel *viewModel = [[CAuctionsViewModel alloc] initWithApiClient:self.mockApiClient];
-    [[viewModel.auctionsObserver deliverOnMainThread]
-     subscribeNext:^(id _) {
-         if (initAuctionsCount < 2){
-             // First two calls are just initialising the array
-             initAuctionsCount++;
-             XCTAssertEqual([viewModel auctionsInSection:0], 0);
-         }
-         else{
-             NSString *name = [viewModel auctionName:[NSIndexPath indexPathForItem:0 inSection:0]];
-             XCTAssertTrue([name isEqualToString:@"foo"]);
-         }
-         [exp fulfill];
-     }];
-    // Wait for the observer
-    [self waitForExpectationsWithTimeout:10
-                                 handler:^(NSError * _Nullable error) {
-                                     XCTAssertNil(error);
-                                 }];
+    [self testWithExpectationCount:3 initHandler:^(CAuctionsViewModel * viewModel){
+        XCTAssertEqual([viewModel auctionsInSection:0], 0);
+    } completion:^(CAuctionsViewModel * viewModel){
+        NSString *name = [viewModel auctionName:[NSIndexPath indexPathForItem:0 inSection:0]];
+        XCTAssertTrue([name isEqualToString:@"foo"]);
+    }];
 }
 
 - (void)testCauctionsViewModelAuctionEra {
-    XCTestExpectation *exp = [self expectationWithDescription:@"testCauctionViewModelAuctionName"];
-    [exp setExpectedFulfillmentCount:3];
-
     NSString *expEra = [NSString stringWithFormat:@"%.2f", (1 + 2.0f - 0.03f - 0.01f) * 20.0f];
     NSString *expEraTxt = [NSString stringWithFormat:Auction_Era_Text, expEra];
 
-    __block NSUInteger initAuctionsCount = 0;
-    CAuctionsViewModel *viewModel = [[CAuctionsViewModel alloc] initWithApiClient:self.mockApiClient];
-    [[viewModel.auctionsObserver deliverOnMainThread]
-     subscribeNext:^(id _) {
-         if (initAuctionsCount < 2){
-             // First two calls are just initialising the array
-             initAuctionsCount++;
-             XCTAssertEqual([viewModel auctionsInSection:0], 0);
-         }
-         else{
-             NSString *era = [viewModel estimatedReturnAmountForAuction:[NSIndexPath indexPathForItem:1 inSection:0]];
-             XCTAssertTrue([era isEqualToString:expEraTxt]);
-         }
-         [exp fulfill];
-     }];
-    // Wait for the observer
-    [self waitForExpectationsWithTimeout:10
-                                 handler:^(NSError * _Nullable error) {
-                                     XCTAssertNil(error);
-                                 }];
+    [self testWithExpectationCount:3 initHandler:^(CAuctionsViewModel * viewModel){
+        XCTAssertEqual([viewModel auctionsInSection:0], 0);
+    } completion:^(CAuctionsViewModel * viewModel){
+        NSString *era = [viewModel estimatedReturnAmountForAuction:[NSIndexPath indexPathForItem:1 inSection:0]];
+        XCTAssertTrue([era isEqualToString:expEraTxt]);
+    }];
 }
 
 @end
