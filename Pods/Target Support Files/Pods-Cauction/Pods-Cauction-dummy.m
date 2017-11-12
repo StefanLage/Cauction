468f1c25c72b35169ff02415294ec58230963221
Stefan Lage <lagestfan@gmail.com>
2017-11-12 16:34:33 +0000

    Remove pods directory from versionning

--- Pods/Target Support Files/Pods-Cauction/Pods-Cauction-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Cauction : NSObject
-@end
-@implementation PodsDummy_Pods_Cauction
-@end
--- Pods/Target Support Files/Pods-CauctionTests/Pods-CauctionTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CauctionTests : NSObject
-@end
-@implementation PodsDummy_Pods_CauctionTests
-@end
